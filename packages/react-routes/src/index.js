6022a285518ff70f50563f01568d4e190a877e6f
Gabe Klein <gabe@termtm.com>
2018-06-06 07:12:40 -0400

    add react routes

---
+++ packages/react-routes/src/index.js
@@ -0,0 +1,47 @@
+export { MemoryRouter } from "react-router-dom"
+
+import { Switch, Route as LibRoute } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
+
+import React from "react"
+
+export const Route = (props) => do {
+    const {
+        children,
+        to,
+        exact,
+        path,
+        callback,
+        render
+    } = props;
+
+    LibRoute(
+        component = callback || children,
+        exact = exact,
+        path = to
+    )
+}
+
+export const AnimateRoutes = (props) => do {
+    const {
+        children,
+        style,
+        states,
+        duration,
+        className,
+        classNames,
+        ...extra
+    } = props;
+
+    LibRoute(render = ({ location }) => do {
+        TransitionGroup >> CSSTransition(...extra), do {
+            key = location.key;
+            classNames = classNames || states || "fade"
+            timeout = duration || 300;
+
+            container(~style, +className) >> 
+                Switch(+location) > 
+                    children;
+        }
+    })
+}
