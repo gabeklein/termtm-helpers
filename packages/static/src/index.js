728893fca3e44d9b564ab1c97bd25d746db15c01
Gabe Klein <gabe@termtm.com>
2018-08-08 23:35:04 -0400

    asset template will allow for insertions

--- packages/static/src/index.js
+++ packages/static/src/index.js
@@ -1,13 +1,21 @@
 const CDN = process.env.CDN;
 
+function noop([strings, ...keys]) {
+  const lastIndex = strings.length - 1;
+  return strings
+    .slice(0, lastIndex)
+    .reduce((p, s, i) => p + s + keys[i], '')
+    + strings[lastIndex];
+};
+
 if(!CDN) throw new Error(`\
     CDN not set in env!
     Need a url.\
 `)
 
-function source(name, ext = ""){
+function source(args, ext = ""){
     let background;
-    name = [].concat(name)[0];
+    let name = noop(args);
     if(name[name.length - 1] == "!"){
         name = name.slice(0, -1);
         background = true;
@@ -17,8 +25,8 @@ function source(name, ext = ""){
     else return name
 }
 
-export const src = (name) => source(name)
-export const png = (name) => source(name, ".png")
-export const svg = (name) => source(name, ".svg")
-export const jpg = (name) => source(name, ".jpg")
-export const mp4 = (name) => source(name, ".mp4")
+export const src = (...args) => source(args)
+export const png = (...args) => source(args, ".png")
+export const svg = (...args) => source(args, ".svg")
+export const jpg = (...args) => source(args, ".jpg")
+export const mp4 = (...args) => source(args, ".mp4")
