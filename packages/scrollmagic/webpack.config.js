96d22f4f91664cd170efc0ecaffa849b6e0cb886
Gabe Klein <gabeklein@Gabes-MacBook-Pro-2190.local>
2019-01-27 09:07:11 -0500

    rearranged entry, no effect

--- packages/scrollmagic/webpack.config.js
+++ packages/scrollmagic/webpack.config.js
@@ -3,9 +3,9 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
+        "ScrollMagic": "scrollmagic/scrollmagic/minified/ScrollMagic.min.js",
         "animation.gsap": "scrollmagic/scrollmagic/minified/plugins/animation.gsap.min.js",
-        "debug.addIndicators": 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js',
-        "ScrollMagic": "scrollmagic/scrollmagic/minified/ScrollMagic.min.js"
+        "debug.addIndicators": 'scrollmagic/scrollmagic/minified/plugins/debug.addIndicators.min.js'
     }, 
     output: {
         filename: "[name].js",
